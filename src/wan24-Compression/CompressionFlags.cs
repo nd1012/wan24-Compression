e9a15817ffff032e4e4d1369c5474834dce67e91
nd <nd1012@users.noreply.github.com>
2024-01-20 10:24:56 +0100

    Update (#29)
    + Updated references

--- src/wan24-Compression/CompressionFlags.cs
+++ src/wan24-Compression/CompressionFlags.cs
@@ -1,4 +1,6 @@
-﻿namespace wan24.Compression
+﻿using wan24.Core;
+
+namespace wan24.Compression
 {
     /// <summary>
     /// Compression flags
@@ -9,22 +11,27 @@
         /// <summary>
         /// None
         /// </summary>
+        [DisplayText("None")]
         None = 0,
         /// <summary>
         /// Serializer version included
         /// </summary>
+        [DisplayText("Serializer version included")]
         SerializerVersionIncluded = 1,
         /// <summary>
         /// Algorithm value included
         /// </summary>
+        [DisplayText("Algorithm value included")]
         AlgorithmIncluded = 2,
         /// <summary>
         /// Uncompressed data length in bytes included
         /// </summary>
+        [DisplayText("Uncompressed data length in bytes included")]
         UncompressedLengthIncluded = 4,
         /// <summary>
         /// All flags
         /// </summary>
+        [DisplayText("All flags")]
         ALL = SerializerVersionIncluded | AlgorithmIncluded | UncompressedLengthIncluded
     }
 }
